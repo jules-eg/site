421b492e5f0842fbab643acc0e1448fc80ff1036
Your Name <you@example.com>
2023-06-29 10:51:43 +0200

    maj sommaire

--- main.js
+++ main.js
@@ -1,42 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   var sommaire = document.querySelector('#sommaire');
   var liensSommaire = sommaire.querySelectorAll('a');
+  var sommaireToggle = document.querySelector('#sommaire-toggle'); // Ajout de l'élément de bascule du sommaire
 
   liensSommaire.forEach(function(lien) {
     lien.addEventListener('click', function(e) {
       e.preventDefault();
       var targetSection = document.querySelector(this.getAttribute('href'));
       var sommaireHeight = sommaire.offsetHeight;
-
-      // Vérifier la vitesse de connexion de l'utilisateur
-      var connectionSpeed = navigator.connection ? navigator.connection.effectiveType : 'unknown';
-      var imageQuality = '';
-
-      // Sélectionner la qualité d'image en fonction de la vitesse de connexion
-      switch (connectionSpeed) {
-        case 'slow-2g':
-        case '2g':
-          imageQuality = 'low';
-          break;
-        case '3g':
-          imageQuality = 'medium';
-          break;
-        case '4g':
-        case '5g':
-          imageQuality = 'high';
-          break;
-        default:
-          imageQuality = 'medium';
-          break;
-      }
-
-      // Mettre à jour les attributs src des images avec la qualité appropriée
-      var images = targetSection.querySelectorAll('img');
-      images.forEach(function(image) {
-        var src = image.getAttribute('src');
-        var newSrc = src.replace('.jpeg', '_' + imageQuality + '.jpeg');
-        image.setAttribute('src', newSrc);
-      });
 
       // Calculer la position de défilement en tenant compte de la hauteur du sommaire
       var scrollToPosition = targetSection.offsetTop - sommaireHeight;
@@ -46,6 +17,13 @@ document.addEventListener('DOMContentLoaded', function() {
         top: scrollToPosition,
         behavior: 'smooth'
       });
+
+      sommaire.classList.remove('active'); // Rétracte le sommaire après un clic sur un lien
     });
+  });
+
+  sommaireToggle.addEventListener('click', function(e) {
+    e.preventDefault();
+    sommaire.classList.toggle('active'); // Affiche ou rétracte le sommaire lors du clic sur l'élément de bascule
   });
 });
