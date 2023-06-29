8e3cdccd0851a5362628c207a1f1543cd66bbf99
Your Name <you@example.com>
2023-06-29 11:54:39 +0200

    maj

--- main.js
+++ main.js
@@ -1,47 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
   var sommaire = document.querySelector('#sommaire');
   var liensSommaire = sommaire.querySelectorAll('a');
-  var sommaireToggle = document.querySelector('#sommaire-toggle'); // Ajout de l'élément de bascule du sommaire
 
+  // Cacher le sommaire
+  function cacherSommaire() {
+    sommaire.style.left = -(sommaire.offsetWidth + 100) + 'px';
+  }
+
+  // Afficher le sommaire
+  function afficherSommaire() {
+    sommaire.style.left = '0';
+  }
+
+  // Ajouter un écouteur d'événement pour le clic sur le sommaire
   liensSommaire.forEach(function(lien) {
     lien.addEventListener('click', function(e) {
       e.preventDefault();
+      cacherSommaire();
       var targetSection = document.querySelector(this.getAttribute('href'));
       var sommaireHeight = sommaire.offsetHeight;
-
+  
       // Calculer la position de défilement en tenant compte de la hauteur du sommaire
       var scrollToPosition = targetSection.offsetTop - sommaireHeight;
-
+  
       // Défiler jusqu'à la position
       window.scrollTo({
         top: scrollToPosition,
         behavior: 'smooth'
       });
-
-      sommaire.classList.remove('active'); // Rétracte le sommaire après un clic sur un lien
-    });
-
-    // Gestion des événements tactiles pour les appareils mobiles
-    lien.addEventListener('touchend', function(e) {
-      e.preventDefault();
-      var targetSection = document.querySelector(this.getAttribute('href'));
-      var sommaireHeight = sommaire.offsetHeight;
-
-      // Calculer la position de défilement en tenant compte de la hauteur du sommaire
-      var scrollToPosition = targetSection.offsetTop - sommaireHeight;
-
-      // Défiler jusqu'à la position
-      window.scrollTo({
-        top: scrollToPosition,
-        behavior: 'smooth'
-      });
-
-      sommaire.classList.remove('active'); // Rétracte le sommaire après un toucher sur un lien
     });
   });
 
-  sommaireToggle.addEventListener('click', function(e) {
-    e.preventDefault();
-    sommaire.classList.toggle('active'); // Affiche ou rétracte le sommaire lors du clic sur l'élément de bascule
+  // Ajouter un écouteur d'événement pour le clic en dehors du sommaire
+  document.addEventListener('click', function(e) {
+    if (!sommaire.contains(e.target)) {
+      afficherSommaire();
+    }
   });
+
+  // Appel initial pour cacher le sommaire
+  cacherSommaire();
 });
