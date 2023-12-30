415b760e2a8e30fe898717e3130593c9930b858b
Jules EVENO laptop <Vous@exemple.com>
2023-12-30 01:59:01 +0100

    t

--- game/main.js
+++ game/main.js
@@ -1,1 +1,13 @@
-window.location.href = base
+function rediriger() {
+    // Récupérer la valeur de l'URL entrée par l'utilisateur
+    var url = document.getElementById('urlInput').value;
+
+    // Vérifier si l'URL est non vide
+    if (url.trim() !== '') {
+        // Rediriger vers l'URL spécifiée
+        window.location.href = url;
+    } else {
+        // Afficher un message d'erreur si l'URL est vide
+        alert('Veuillez entrer une URL valide.');
+    }
+}
