ce4c17c3349a06f85aace68026a59fdae4a09edd
Jules EVENO laptop <Vous@exemple.com>
2024-01-05 15:51:29 +0100

    maj

--- game/script.sh
+++ game/script.sh
@@ -3,7 +3,34 @@ for ((a=0; a<10; a++))
     do mkdir $a
     for ((b=0; b<10; b++))
         do mkdir $a/$b
-        
+        for ((c=0; c<10; c++))
+            do mkdir $a/$b/$c
+            for ((d=0; d<10; d++))
+                do mkdir $a/$b/$c/$d
+                for ((e=0; e<10; e++))
+                    do mkdir $a/$b/$c/$d/$e
+                    for ((f=0; f<10; f++))
+                        do mkdir $a/$b/$c/$d/$e/$f
+                        for ((g=0; g<10; g++))
+                            do mkdir $a/$b/$c/$d/$e/$f/$g
+                            for ((h=0; h<10; h++))
+                                do mkdir $a/$b/$c/$d/$e/$f/$g/$h
+                                for ((i=0; i<10; i++))
+                                    do mkdir $a/$b/$c/$d/$e/$f/$g/$h/$i
+                                    for ((j=0; j<10; j++))
+                                        do mkdir $a/$b/$c/$d/$e/$f/$g/$h/$i/$j
+                                        for ((k=0; k<10; k++))
+                                            do mkdir $a/$b/$c/$d/$e/$f/$g/$h/$i/$j/$k
+                                            for ((l=0; l<10; l++))
+                                                do mkdir $a/$b/$c/$d/$e/$f/$g/$h/$i/$j/$k/$l
+                                                for ((m=0; m<10; m++))
+                                                    do mkdir $a/$b/$c/$d/$e/$f/$g/$h/$i/$j/$k/$l/$m
+                                                    for ((n=0; n<10; n++))
+                                                        do mkdir $a/$b/$c/$d/$e/$f/$g/$h/$i/$j/$k/$l/$m/$n
+                                                        for ((o=0; o<10; o++))
+                                                            do mkdir $a/$b/$c/$d/$e/$f/$g/$h/$i/$j/$k/$l/$m/$n/$o
+                                                            for ((p=0; p<10; p++))
+                                                                do mkdir $a/$b/$c/$d/$e/$f/$g
 
 
 
