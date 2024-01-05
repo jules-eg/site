fa14ca08c98ab3afc029e7b2f9e2d7763867599d
Jules EVENO laptop <Vous@exemple.com>
2024-01-05 15:54:32 +0100

    maj

--- game/script.sh
+++ game/script.sh
@@ -14,20 +14,4 @@ for ((a=0; a<10; a++))
                         for ((g=0; g<10; g++))
                             do mkdir $a/$b/$c/$d/$e/$f/$g
                             for ((h=0; h<10; h++))
-                                do mkdir $a/$b/$c/$d/$e/$f/$g/$h
-                                for ((i=0; i<10; i++))
-                                    do mkdir $a/$b/$c/$d/$e/$f/$g/$h/$i
-                                    for ((j=0; j<10; j++))
-                                        do cp base $a/$b/$c/$d/$e/$f/$g/$h/$i/$j
-                                        echo $a$b$c$d$e$f$g$h$i$j >> $a/$b/$c/$d/$e/$f/$g/$h/$i/$j
-                                        echo $a$b$c$d$e$f$g$h$i$j
-                                    done
-                                done
-                            done
-                        done
-                    done
-                done
-            done
-        done
-    done
-done
+                                
