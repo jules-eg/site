2d6ad669ff409d993fcad525b1403a355afa39cd
Jules EVENO laptop <Vous@exemple.com>
2024-01-05 15:52:37 +0100

    maj

--- game/script.sh
+++ game/script.sh
@@ -28,4 +28,3 @@ for ((a=0; a<10; a++))
 
 
 
-; do mkdir $a ;  for ((b=0;b<10;b++)) ; do mkdir $a/$b ; for ((c=0 ; c<10; c++)) ; do mkdir $a/$b/$c ;  for ((d=0;d<10;d++)) ;do mkdir $a/$b/$c/$d ; for ((e=0;e<10;e++)) ; do m^Cp base  $a/$b/$c/$d/$e ; echo $a/$b/$c/$d/$e >> $a/$b/$c/$d/$e ; echo $a$b$c$d$e ;done ; done ;done ;done ;done;done;done;done
